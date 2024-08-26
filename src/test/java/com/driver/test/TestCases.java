6fc94980f0f8c4c25288c4a585e404bb853ae178
Mukul Vasanth <mukulvasanth@gmail.com>
2024-08-26 19:07:44 +0530

    Updated method overriding changes

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,17 @@
 package com.driver.test;
 
 import static org.junit.Assert.*;
+
+import com.driver.B;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+    @Test
+    public void testMethodOverriding() {
+        B b = new B();
+
+        String result  = b.meth();
+
+        assertEquals("Method is overridden in extended class B", result);
+    }
 }
